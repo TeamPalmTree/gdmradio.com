1cd99a1c16d681ba03b94a94984b7eb113d33635
Alex Groleau <alex@teampalmtree.com>
2013-10-09 22:35:21 -0400

    updates

--- public/assets/js/template.js
+++ public/assets/js/template.js
@@ -10,13 +10,23 @@ var gdmradio_playing_model = function () {
 
     // poll
     this.poll = function() {
-        $.get('http://cloudcast.gdmradio.com/engine/status.json', null, function (status_js) {
-            if (!this.status())
-                this.status(new status_model(status_js));
-            else
-                this.status().update(status_js);
-        }.bind(this));
+        $.ajax({
+            url: 'http://cloudcast.gdmradio.com/engine/status.json',
+            dataType: "json",
+            success: function (status_js) {
+                if (!this.status())
+                    this.status(new status_model(status_js));
+                else
+                    this.status().update(status_js);
+            }.bind(this)
+        });
     }.bind(this);
+
+    // listen
+    this.listen = function() {
+        window.location = "#gdmradio-listen"
+        $('#gdmradio-listen').effect('pulsate');
+    };
 
     // poll engine for status
     setInterval(function() {
@@ -37,40 +47,6 @@ var status_model = function (status_js) {
     // standard members
     this.current_file_artist = ko.observable();
     this.current_file_title = ko.observable();
-    this.current_file_duration = ko.observable();
-    this.current_file_post = ko.observable();
-    this.next_file_artist = ko.observable();
-    this.next_file_title = ko.observable();
-    this.current_show_title = ko.observable();
-    this.current_show_duration = ko.observable();
-    this.next_show_title = ko.observable();
-    this.current_client_schedule_start_on = ko.observable();
-    this.current_client_schedule_file_played_on = ko.observable();
-    this.host_username = ko.observable();
-    this.client_generated_on = ko.observable();
-    // additional members
-    this.current_file_percentage = ko.observable();
-    this.current_show_percentage = ko.observable();
-    this.current_file_post_percentage = ko.observable();
-    this.current_file_elapsed = ko.observable();
-    this.current_file_remaining = ko.observable();
-    this.current_show_elapsed = ko.observable();
-    this.current_show_remaining = ko.observable();
-    this.updated_on_time = ko.observable();
-    // input statuses
-    this.schedule_input_active = ko.observable();
-    this.show_input_active = ko.observable();
-    this.talkover_input_active = ko.observable();
-    this.master_input_active = ko.observable();
-    // input enableds
-    this.schedule_input_enabled = ko.observable();
-    this.show_input_enabled = ko.observable();
-    this.talkover_input_enabled = ko.observable();
-    this.master_input_enabled = ko.observable();
-    // input usernames
-    this.show_input_username = ko.observable();
-    this.talkover_input_username = ko.observable();
-    this.master_input_username = ko.observable();
 
     // update
     this.update = function(status_js) {
@@ -90,14 +66,7 @@ var status_model = function (status_js) {
 function hook_gdmradio() {
 
     // playing
-    //ko.applyBindings(new gdmradio_playing_model(), document.getElementById('gdmradio-playing'));
-
-    // listen
-    $('#gdmradio-playing-listen').click(function() {
-        window.location = "#gdmradio-listen"
-        $('#gdmradio-listen').effect('pulsate');
-    });
-
+    ko.applyBindings(new gdmradio_playing_model(), document.getElementById('gdmradio-playing'));
     // carousel
     $('.carousel').carousel({
         interval: 5000
